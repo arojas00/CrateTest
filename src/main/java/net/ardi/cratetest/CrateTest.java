7888f4877c67d9050f05534870fa0da14788e50d
Adrián Rojas <adrian.rojas@sunnaentertainment.com>
2024-08-06 09:52:25 -0600

    Initial commit

---
+++ src/main/java/net/ardi/cratetest/CrateTest.java
@@ -0,0 +1,14 @@
+package net.ardi.cratetest;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CrateTest implements ModInitializer {
+	public static final String MOD_ID = "cratetest";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/net/ardi/cratetest/CrateTestClient.java
@@ -0,0 +1,10 @@
+package net.ardi.cratetest;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CrateTestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
